d1b92819a272caada2f602b7d2c8b6d1166b97ee
codespirit7 <itsriteshoff@gmail.com>
2023-04-11 17:07:16 +0530

    node script

---
+++ index.js
@@ -0,0 +1,20 @@
+var Tesseract = require("tesseract.js");
+var request = require("request");
+var fs = require("fs");
+var url = " https://i.ibb.co/jTKYQqP/Captcha-United.png";
+var filename = "file.png";
+
+var writeFile = fs.createWriteStream(filename);
+
+request(url)
+  .pipe(writeFile)
+  .on("close", function () {
+    console.log(url, "saved to", filename);
+
+    Tesseract.recognize(filename)
+      .catch((err) => console.error(err))
+      .then(function (result) {
+        console.log(result.data.text);
+        process.exit(0);
+      });
+  });
